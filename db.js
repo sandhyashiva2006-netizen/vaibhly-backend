52ce2518c3255472a9cfb6d8357dc875eb26407a
sandhyashiva2006-netizen <sandhyashiva2006@gmail.com>
2026-04-21 00:29:46 +0530

    update db

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const { Pool } = require("pg");
 
+console.log("DATABASE_URL FOUND:", !!process.env.DATABASE_URL);
+
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: process.env.DB_PASSWORD,
-  database: "edunexa_db",
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+pool.connect()
+.then(() => console.log("✅ PostgreSQL Connected"))
+.catch(err => console.error("❌ DB ERROR:", err));
 
 module.exports = pool;
